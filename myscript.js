5697df57516dd1f81bfef7fba154df0cc941de3f
Reuben <imbapalagz@gmail.com>
2020-10-19 17:58:23 +0800

    second commit, just styling changes

--- myscript.js
+++ myscript.js
@@ -1,14 +1,6 @@
-function Book(title, author, pages, hasBeenRead) {
-    this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.hasBeenRead = hasBeenRead;
-	this.info = function() {
-		let readText = 'not read yet';
-		if (this.hasBeenRead) {
-			readText = 'already read';
-		}
-		
-		return title + ' by' + author + ', ' + pages + ' pages, ' + readText;
-	}
-}
+const nav = window.getComputedStyle(document.querySelector('nav'));
+const recBooksSection = window.getComputedStyle(document.querySelector('#recommended-books'));
+let screenHeight = window.innerHeight;
+let booksListSectionHeight = (screenHeight - (parseInt(recBooksSection.height, 10) + parseInt(nav.height, 10))) - 25;
+
+document.querySelector('#books-list').style.height = booksListSectionHeight + 'px';
