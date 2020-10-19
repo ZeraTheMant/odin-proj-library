78573770b22e672801bc3fd51ff5a28568a1bce7
Reuben <imbapalagz@gmail.com>
2020-10-19 18:40:12 +0800

    final commit for 19 oct 2020, design updates

--- myscript.js
+++ myscript.js
@@ -1,6 +1,18 @@
+function addBook() {
+    emptyLibraryMsgObj.style.display = 'none';
+    booksListContainer.style.display = 'block';
+    
+}
+
 const nav = window.getComputedStyle(document.querySelector('nav'));
 const recBooksSection = window.getComputedStyle(document.querySelector('#recommended-books'));
 let screenHeight = window.innerHeight;
 let booksListSectionHeight = (screenHeight - (parseInt(recBooksSection.height, 10) + parseInt(nav.height, 10))) - 25;
+let emptyLibraryMsgObj = document.querySelector('#empty-list-msg');
+let booksListContainer = document.querySelector('#books-list-outer-container');
 
 document.querySelector('#books-list').style.height = booksListSectionHeight + 'px';
+
+
+const addBookBtn = document.querySelector('#add-book-btn');
+addBookBtn.addEventListener('click', addBook);
