39363c41bf9859f08696a81b4f3c53760844cd3b
Reuben <imbapalagz@gmail.com>
2020-10-22 17:45:37 +0800

    final version

--- myscript.js
+++ myscript.js
@@ -6,7 +6,7 @@ function addBookFormComplete() {
 function addBookToList(book) {
     const libraryItemDiv = document.createElement('div');
     libraryItemDiv.setAttribute('class', 'recommended-book-item library-item');
-    libraryItemDiv.className = 'recommended-book-item library-item';
+    libraryItemDiv.className = 'recommended-book-item library-item relative';
     
     const relativeDiv = document.createElement('div');
     const bookImg = document.createElement('img');
@@ -16,10 +16,11 @@ function addBookToList(book) {
     relativeDiv.appendChild(bookImg);
     
     const infoDiv = document.createElement('div');
+
     const titleHeader = document.createElement('h4');
     titleHeader.textContent = book.title;
     const authorElem = document.createElement('p');
-    authorElem.textContent = book.author + ' - ' + book.pages;
+    authorElem.textContent = book.author + ' - ' + book.pages + ' pages';
     authorElem.className = 'rec-book-title';
     const descElem = document.createElement('p');
     descElem.textContent = book.desc;
@@ -32,9 +33,51 @@ function addBookToList(book) {
     infoDiv.appendChild(authorElem);
     infoDiv.appendChild(descElem);
     
+    const bookBtnsDiv = document.createElement('div');
+    bookBtnsDiv.className = 'read-and-del-btns-container';
+    const bookStatus = document.createElement('span');
+    let bookClass = 'absolute book-btns seen-book';
+    
+    if (!book.hasBeenRead) {
+        bookClass = 'absolute book-btns unseen-book';
+    }
+       
+    bookStatus.className = bookClass;
+    bookStatus.innerHTML = '&nbsp';
+    bookStatus.addEventListener('click', changeBookStatus);
+    const deleteBook = document.createElement('span');
+    deleteBook.className = 'absolute book-btns del-book';
+    deleteBook.innerHTML = '&nbsp';
+    deleteBook.addEventListener('click', removeBook);
+    
+    bookBtnsDiv.append(bookStatus);
+    bookBtnsDiv.append(deleteBook);
+    
     libraryItemDiv.appendChild(relativeDiv);
     libraryItemDiv.appendChild(infoDiv);
+    libraryItemDiv.appendChild(bookBtnsDiv);
     document.querySelector('#books-list-inner-container').appendChild(libraryItemDiv);
+}
+
+function changeBookStatus(e) {
+    if (e.target.classList.contains('seen-book')) {
+        e.target.className = 'absolute book-btns unseen-book';
+    } else {
+        e.target.className = 'absolute book-btns seen-book';
+    }
+}
+
+function removeBook(e) {
+    const bookTitle = e.target.parentNode.previousSibling.firstChild.textContent; 
+    const willDelete = confirm('Delete ' + bookTitle + '?');
+
+    if (willDelete) {
+        const booksContainer = e.target.parentNode.parentNode.parentNode;
+        const itemToBeDeleted = e.target.parentNode.parentNode;
+        
+        booksContainer.removeChild(itemToBeDeleted);
+        alert('Book deleted successfully.'); 
+    }
 }
 
 function addBook() {
